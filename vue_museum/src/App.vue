ecebdc3bdc9b05db7671b1f2fef07399966192d5
“Liucxiong” <3309296020@qq.com>
2025-05-04 22:48:57 +0800

    newchange

--- vue_museum/src/App.vue
+++ vue_museum/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/login">Login</router-link>
+  </nav> -->
   <router-view/>
 </template>
+
 
 <style>
 #app {
@@ -14,16 +16,13 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
-  padding: 30px;
+  padding: 16px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
 }
