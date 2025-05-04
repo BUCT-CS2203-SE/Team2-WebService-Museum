4f14b938cab99b4f488efbaedbf692a0a674fa7f
“Liucxiong” <3309296020@qq.com>
2025-05-04 22:49:46 +0800

    路由

--- vue_museum/src/router/index.js
+++ vue_museum/src/router/index.js
@@ -1,24 +1,23 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Login from '@/views/toLogin.vue';
 
 const routes = [
   {
+    // 根目录
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: () => import('../views/HomeView.vue'),
     meta: { public: true }
   },{
+    //登录页
     path: '/login',
-    component: Login,
+    component: () => import('@/views/toLogin.vue'),
     meta: { public: true }
   },{
+    //信息页
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    // 懒加载（推荐使用）
+    component: () => import('../views/AboutView.vue'),
     meta: { auth: true } 
   }
 ]
@@ -35,7 +34,7 @@ router.beforeEach((to, from, next) => {
   // 需要认证但无 token，跳登录
   const token = localStorage.getItem('jwt');
   if (to.meta.auth && !token) {
-    console.log("重定向");
+    console.log("路由重定向至登陆界面");
     return next('/login');
   }
   // 其余情况放行
